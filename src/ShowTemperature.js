438fb8bdbe940fb40d5916f52f82a40ec2b7e6f0
Mahsa Irankhah <m.irankhah1995@gmail.com>
2022-03-14 02:10:22 +0330

    removed convert unit links

--- src/ShowTemperature.js
+++ src/ShowTemperature.js
@@ -1,44 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from "./WeatherInfo.module.css";
 
 const ShowTemperature = (props) => {
-    let [unit, setUnit] = useState('metric');
-    function convertToF(event) {
-       event.preventDefault();
-       setUnit('imperial');
-    }
-    function converToCelsius(event) {
-        event.preventDefault();
-        setUnit('metric')
-    }
-
-    if (unit === 'metric') {
+    
         return (
           <div>
             <span className={styles.temp}>{props.temp}</span>
             <span className={styles.unitLinks}>
-              °C |{" "}
-              <a href="/" onClick={convertToF}>
-                °F
-              </a>
-            </span>
-          </div>
-        );
-    } else {
-      let fTemp = Math.round((props.temp * 9) / 5) + 32;
-        return (
-          <div>
-            <span className={styles.temp}>{fTemp}</span>
-            <span className={styles.unitLinks}>
-              <a href="/" onClick={converToCelsius}>
-                °C
-              </a>{" "}
-              | °F
+              °C
             </span>
           </div>
         );
     }
     
-};
 
 export default ShowTemperature;
